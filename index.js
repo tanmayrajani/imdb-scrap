b1b497cb5118b07e29dd518735e62cbfbd846ff7
Tanmay Rajani <rajani.tanmay@gmail.com>
2016-01-10 21:08:25 +0530

    added folder name functionality

--- index.js
+++ index.js
@@ -5,46 +5,48 @@ var request = require("request"),
 	path = require('path'),
 	sc = [];
 if(process.argv[2]) nm = process.argv[2].toString();
-var url1 = "http://www.imdb.com/name/"+nm+"/mediaindex" ;
+var mainUrl = "http://www.imdb.com/name/"+nm+"/mediaindex" ;
 var number;
+var celName="";
 
-request(url1, function (error, response, body) {
+request(mainUrl, function (error, response, body) {
 	if (!error) {
 		var $ = cheerio.load(body),
-		num = $('.desc').first().text();
+		num = $('.desc').first().text(),
+		celName = $('h3 a').first().text();
 		number = num.substring(num.indexOf('of')+3,num.lastIndexOf(0)+1);
-		var nextUrl1 = 'http://imdb.com' + $('.media_index_thumb_list a:nth-child(1)').attr('href');
-		console.log(nextUrl1)
-		everyFuckingPhoto(nextUrl1, downloadingImage);
+		var nextUrl = 'http://imdb.com' + $('.media_index_thumb_list a:nth-child(1)').attr('href');
+		console.log(nextUrl)
+		everyFuckingPhoto(nextUrl, celName, downloadingImage);
 	} else {
 		console.log("Mood nathi yaar: " + error);
 	}
 });
 
-function everyFuckingPhoto (url2, callback) {
+function everyFuckingPhoto (url, celName, callback) {
 	if(number-- == 0) return;
-	request(url2, function (error, response, body) {
+	request(url, function (error, response, body) {
 		if (!error) {
 			var $ = cheerio.load(body),
-			nextUrl2 = 'http://imdb.com' + $('#right a:nth-child(3)').attr('href'),
-			imgUrl1 = $('.photo a img').attr('src');
-			console.log(nextUrl2);
-			callback(imgUrl1, nextUrl2);
+			nextUrl = 'http://imdb.com' + $('#right a:nth-child(3)').attr('href'),
+			imgUrl = $('.photo a img').attr('src');
+			console.log(nextUrl);
+			callback(imgUrl, celName, nextUrl);
 		}
 	})
 }
 
-function downloadingImage (imgUrl2, next) {
-	request(imgUrl2)
+function downloadingImage (imgUrl, celName, next) {
+	request(imgUrl)
 		.on('response',  function (res) {
 			mkdirSync(path.join('media'));
-  			mkdirSync(path.join('media',nm));
-  			res.pipe(fs.createWriteStream('./media/'+nm+'/'+nm+'-'+number+'.jpg'));	
+  			mkdirSync(path.join('media',celName));
+  			res.pipe(fs.createWriteStream('./media/'+celName+'/'+nm+'-'+number+'.jpg'));	
 		})
 		.on('error', function (err) {
 			console.log(err);
 		});
-	everyFuckingPhoto(next, downloadingImage);
+	everyFuckingPhoto(next, celName, downloadingImage);
 }
 
 function mkdirSync (path) {
