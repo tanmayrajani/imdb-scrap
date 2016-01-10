21d36f03783f1dd70f18cf8ae1d90c925bc33b21
Tanmay Rajani <rajani.tanmay@gmail.com>
2016-01-10 16:42:28 +0530

    working perfectly :punch:

--- index.js
+++ index.js
@@ -3,49 +3,42 @@ var request = require("request"),
 	cheerio = require("cheerio"),
 	fs = require('fs'),
 	sc = [],
-	url = "http://www.imdb.com/name/"+nm+"/mediaindex?page=" ;
+	url1 = "http://www.imdb.com/name/"+nm+"/mediaindex?page=" ;
 var number;
 
-request(url, function (error, response, body) {
+request(url1, function (error, response, body) {
 	if (!error) {
 		var $ = cheerio.load(body),
 		num = $('.desc').first().text();
 		number = num.substring(num.indexOf('of')+3,num.lastIndexOf(0)+1);
-		var nextUrl = 'http://imdb.com' + $('.media_index_thumb_list a:nth-child(1)').attr('href');
-		console.log(nextUrl)
-		console.log(number)
-
-		everyFuckingPhoto(nextUrl);
-
+		var nextUrl1 = 'http://imdb.com' + $('.media_index_thumb_list a:nth-child(1)').attr('href');
+		console.log(nextUrl1)
+		everyFuckingPhoto(nextUrl1, downloadingImage);
 	} else {
 		console.log("Mood nathi yaar: " + error);
 	}
 });
 
-function everyFuckingPhoto (url) {
-	request(url, function (error, response, body) {
+function everyFuckingPhoto (url2, callback) {
+	if(number-- == 0) return;
+	request(url2, function (error, response, body) {
 		if (!error) {
 			var $ = cheerio.load(body),
-			source = $('.photo a img').attr('src'),
-			nextUrl = 'http://imdb.com' + $('#right a').first().attr('href'),
-			imgUrl = $('.photo a img').attr('src');
-			console.log(imgUrl);
-			downloadingImage(imgUrl);
-			// get the fucking image 
-			if(number--) everyFuckingPhoto(nextUrl)
-		}
-		else{
-
+			nextUrl2 = 'http://imdb.com' + $('#right a:nth-child(3)').attr('href'),
+			imgUrl1 = $('.photo a img').attr('src');
+			console.log(nextUrl2);
+			callback(imgUrl1, nextUrl2);
 		}
 	})
 }
 
-function downloadingImage (imgUrl) {
-	request(imgUrl)
-			.on('response',  function (res) {
-  				res.pipe(fs.createWriteStream('./media/jessica-'+number+'.jpg'));
-			})
-			.on('error', function (err) {
-				console.log(err);
-			});
+function downloadingImage (imgUrl2, next) {
+	request(imgUrl2)
+		.on('response',  function (res) {
+  			res.pipe(fs.createWriteStream('./media/'+nm+'-'+number+'.jpg'));
+		})
+		.on('error', function (err) {
+			console.log(err);
+		});
+	everyFuckingPhoto(next, downloadingImage);
 }
