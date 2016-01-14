57f73c6a7f44493a38ba35bfc279fed58de2e97d
Tanmay Rajani <rajani.tanmay@gmail.com>
2016-01-14 08:45:24 +0530

    removed strong words

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-var nm = "nm1567113";	// Jessica Chastain, bitches! <3
+var nm = "nm1567113";	// Jessica Chastain! <3
 var request = require("request"),
 	cheerio = require("cheerio"),
 	fs = require('fs'),
 	path = require('path');
 
-if(process.argv[2]) nm = process.argv[2].toString();  // Just in case, if you don't like Jessica.. you know
-var mainUrl = "http://www.imdb.com/name/"+nm+"/mediaindex" ;
-var number;
-var celName="";
+if(process.argv[2]) nm = process.argv[2].toString();  // Just in case, if you don't like Jessica.. you know :D
+var mainUrl = "http://www.imdb.com/name/"+nm+"/mediaindex" ,
+number,
+celName="";
 
 request(mainUrl, function (error, response, body) {
 	if (!error) {
 		var $ = cheerio.load(body),
 		num = $('.desc').first().text(),
-		celName = $('h3 a').first().text();
+		celName = $('h3 a').first().text();  // Getting Celebrity Name For folder-name
 		number = num.substring(num.indexOf('of')+3,num.lastIndexOf(" "));
 		var nextUrl = 'http://imdb.com' + $('.media_index_thumb_list a:nth-child(1)').attr('href');
 		console.log(nextUrl);
-		everyFuckingPhoto(nextUrl, celName, downloadingImage);
+		getEveryPhoto(nextUrl, celName, downloadingImage);
 	} else {
-		console.log("Mood nathi yaar: " + error);
+		console.log("Error requesting main URL: " + error);
 	}
 });
 
-function everyFuckingPhoto (url, celName, callback) {
+function getEveryPhoto (url, celName, callback) {
 	if(number-- == 0) return;
 	request(url, function (error, response, body) {
 		if (!error) {
@@ -47,7 +47,7 @@ function downloadingImage (imgUrl, celName, next) {
 			console.log(err);
 		});
 
-	everyFuckingPhoto(next, celName, downloadingImage);
+	getEveryPhoto(next, celName, downloadingImage);
 }
 
 function mkdirSync (path) {
